8840c52ba471c4412322db8d03aeeab5b5977b39
laharumi <laharumi@users.noreply.github.com>
2015-12-26 02:44:07 -0200

    Create and2.vhdl

---
+++ and2.vhdl
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity and2 is
+port ( 
+  input1, input2 : in std_logic;
+  output1        : out std_logic
+  );
+end and2;
+
+architecture arch of and2 is
+begin
+  output1 <= input1 and input2;
+end architecture arch;
