a3cabff7df2b625a2a24480f326534219eb6a5c1
laharumi <laharumi@users.noreply.github.com>
2015-12-26 02:34:12 -0200

    Create or3.vhdl

---
+++ or3.vhdl
@@ -0,0 +1,14 @@
+library ieee;
+use ieee.std_logic_1164;
+
+entity or3 is
+port (
+  input1, input2, input3 : in std_logic;
+  output                 : out std_logic
+  );
+end or3;
+
+architecture arch of or3 is
+begin
+  output <= input1 or input2 or input3;
+end architecture arch;
